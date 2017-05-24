1e973639e3e626f0d69df2888b0280a20b649c91
wjj2329 <wjj2329@gmail.com>
2017-05-24 10:34:49 -0600

    hi

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,15 @@ public class Main
         {
             String s=connection.sendWhere();
             System.out.println(s);
-           //Decoder.setMyMapsFromJson(r, s);
+            Decoder.setMyMapsFromJson(r, s);
 
            Thread.sleep(3000);
-           //int directions[] = r.calculateSpeeds();
-          // String speedResult = connection.sendSpeed(directions[0], directions[1]);
-           //if(directions[0] == 0 && directions[1] == 0)
+           int directions[] = r.calculateSpeeds();
+           String speedResult = connection.sendSpeed(directions[0], directions[1]);
+           if(directions[0] == 0 && directions[1] == 0)
            {
-             //  connection.shutdown();
-               //return;
+               connection.shutdown();
+               return;
            }
 
         }
