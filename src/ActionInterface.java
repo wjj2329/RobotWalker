3ae394308b47999547917a8c645608a8ee885072
MasqueradeOfSilence <alexcn711@gmail.com>
2017-05-24 10:41:16 -0600

    I am a shame on the career of Scott Woodfield

--- src/ActionInterface.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Alex on 5/16/17.
- * Interface designed to make the robot move.
- */
-public interface ActionInterface
-{
-
-}
--- src/VisionInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Alex on 5/16/17.
- * Interface designed to interact with the
- *  visual input received.
- */
-public interface VisionInterface
-{
-    
-}
