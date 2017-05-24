8a35754670f0315f43aba55963b49c5bee22a266
wjj2329 <wjj2329@gmail.com>
2017-05-24 10:52:45 -0600

    hi

--- src/TestCase/TestCaseForMap.java
+++ src/TestCase/TestCaseForMap.java
@@ -1,7 +1,30 @@
 package TestCase;
 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by williamjones on 5/24/17.
  */
 public class TestCaseForMap {
+
+
+
+    @Before
+    public void  setup()
+    {
+
+    }
+    @After
+    public void teardown()
+    {
+
+    }
+    @Test
+    public void testing()
+    {
+
+    }
 }
