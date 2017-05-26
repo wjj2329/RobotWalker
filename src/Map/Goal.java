e1fb8b70ee7e1aad0a129514b16744f1e060c589
MasqueradeOfSilence <alexcn711@gmail.com>
2017-05-25 19:36:32 -0600

    corners

--- src/Map/Goal.java
+++ src/Map/Goal.java
@@ -18,10 +18,20 @@ public class Goal
      */
     private Coordinate center;
 
+    private Coordinate corner1;
+    private Coordinate corner2;
+    private Coordinate corner3;
+    private Coordinate corner4;
+
+
     public Goal(Obstacle o)
     {
         radius = o.getRadius();
         center = o.getCenter(); // type of copy?
+        corner1 = o.getCorner1();
+        corner2 = o.getCorner2();
+        corner3 = o.getCorner3();
+        corner4 = o.getCorner4();
     }
 
     public double getRadius()
@@ -32,5 +42,45 @@ public class Goal
     public Coordinate getCenter()
     {
         return center;
+    }
+
+    public Coordinate getCorner1()
+    {
+        return corner1;
+    }
+
+    public void setCorner1(Coordinate corner1)
+    {
+        this.corner1 = corner1;
+    }
+
+    public Coordinate getCorner2()
+    {
+        return corner2;
+    }
+
+    public void setCorner2(Coordinate corner2)
+    {
+        this.corner2 = corner2;
+    }
+
+    public Coordinate getCorner3()
+    {
+        return corner3;
+    }
+
+    public void setCorner3(Coordinate corner3)
+    {
+        this.corner3 = corner3;
+    }
+
+    public Coordinate getCorner4()
+    {
+        return corner4;
+    }
+
+    public void setCorner4(Coordinate corner4)
+    {
+        this.corner4 = corner4;
     }
 }
